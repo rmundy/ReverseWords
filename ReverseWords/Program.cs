991f8d499ec1e87db206061bee3e183dac3c7f7b
Bobby Mundy <robert.l.mundy@gmail.com>
2016-01-30 09:33:43 -0500

    Initial checkin

---
+++ ReverseWords/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+
+namespace ReverseWords
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public sealed class Program
+    {
+        public static void Main(String[] args)
+        {
+            using (var fs = File.Open(args[0], FileMode.Open))
+            {
+                using (var bs = new BufferedStream(fs))
+                using (var sr = new StreamReader(bs))
+                {
+                    String line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        
+                    }
+                }
+            }
+
+        }
+    }
+}
